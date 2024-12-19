6f0c6747387ca8d7bb0b5e04785758579ee10bd9
0xearlier <Gj2U9IhVwX0@jiangytiuo.com>
2024-12-19 18:20:26 +0300

    Fix issue with Redis cache timeout

--- src/connectors/storage/redis_cache.py
+++ src/connectors/storage/redis_cache.py
@@ -2,11 +2,9 @@ import aioredis
 
 class RedisCache:
     def __init__(self, url: str, ttl: int):
-        self.redis = aioredis.from_url(url)
+        
         self.ttl = ttl
-        
+        self.redis = aioredis.from_url(url)
     async def get(self, key: str):
-        return await self.redis.get(key)
         
-    async def set(self, key: str, value: Any):
-        await self.redis.set(key, value, ex=self.ttl)
+        return await self.redis.get(key)
