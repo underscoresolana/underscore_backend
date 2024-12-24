0952f8c7b3cc8a47945d471782478a79aa710364
lazyahhhhhacker <basechain@mail.com>
2024-12-24 07:16:26 +0300

    [FIX] fix feature extractor for new token types

--- src/core/processing/feature_extractor.py
+++ src/core/processing/feature_extractor.py
@@ -7,18 +7,13 @@ class AdvancedFeatureGenerator:
     """Generates complex features from raw transaction data"""
     
     def __init__(self, degree: int = 2):
-        self.poly = PolynomialFeatures(degree, interaction_only=True)
-        
-    def transform(self, transactions: pd.DataFrame) -> np.ndarray:
-        base_features = transactions[['value', 'block', 'signer_count']]
-        poly_features = self.poly.fit_transform(base_features)
-        time_features = self._extract_time_features(transactions['timestamp'])
         return np.hstack([poly_features, time_features])
+        poly_features = self.poly.fit_transform(base_features)
+    def transform(self, transactions: pd.DataFrame) -> np.ndarray:
+        
+        self.poly = PolynomialFeatures(degree, interaction_only=True)
+        time_features = self._extract_time_features(transactions['timestamp'])
+        base_features = transactions[['value', 'block', 'signer_count']]
         
     def _extract_time_features(self, timestamps: pd.Series) -> np.ndarray:
         dt = pd.to_datetime(timestamps, unit='s')
-        return np.column_stack([
-            dt.dt.hour,
-            dt.dt.dayofweek,
-            dt.dt.is_month_start.astype(int)
-        ])
