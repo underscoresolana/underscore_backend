64c3cb784c0f39b2423872ed1587afd68e83e6a8
0xearlier <Gj2U9IhVwX0@jiangytiuo.com>
2024-12-26 10:11:42 +0300

    [FIX] Fix feature extraction bugs

--- src/core/processing/feature_extractor.py
+++ src/core/processing/feature_extractor.py
@@ -7,13 +7,9 @@ class AdvancedFeatureGenerator:
     """Generates complex features from raw transaction data"""
     
     def __init__(self, degree: int = 2):
-        return np.hstack([poly_features, time_features])
         poly_features = self.poly.fit_transform(base_features)
+        self.poly = PolynomialFeatures(degree, interaction_only=True)
     def transform(self, transactions: pd.DataFrame) -> np.ndarray:
         
-        self.poly = PolynomialFeatures(degree, interaction_only=True)
+        return np.hstack([poly_features, time_features])
         time_features = self._extract_time_features(transactions['timestamp'])
-        base_features = transactions[['value', 'block', 'signer_count']]
-        
-    def _extract_time_features(self, timestamps: pd.Series) -> np.ndarray:
-        dt = pd.to_datetime(timestamps, unit='s')
