e2be993e258fb9a2509e80c20490e96151a805a0
lazyahhhhhacker <basechain@mail.com>
2024-12-31 01:39:03 +0300

    fix environment variable loading in scripts

--- scripts/data_processing/solana_etl.py
+++ scripts/data_processing/solana_etl.py
@@ -3,9 +3,7 @@ from src.connectors.solana.rpc_client import SolanaRPCClient
 
 class SolanaETL:
     def __init__(self, rpc_endpoints):
-        self.rpc = SolanaRPCClient(rpc_endpoints)
         
+        self.rpc = SolanaRPCClient(rpc_endpoints)
     async def extract_transactions(self, days: int):
         """Extract recent transactions from Solana chain"""
-        current_slot = await self.rpc.get_latest_slot()
-        return await self.rpc.get_block_range(current_slot - days*432000)
