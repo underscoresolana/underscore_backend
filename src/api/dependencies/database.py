9b629c6ca22ebf946f4ca6d4a97168fb4a639862
nickverdoodt <6S6kuVbCpHA@jiangytiuo.com>
2025-01-02 05:25:36 +0300

    fix API dependency injection

--- src/api/dependencies/database.py
+++ src/api/dependencies/database.py
@@ -4,5 +4,3 @@ from src.connectors.storage.postgres_client import PostgresClient
 async def get_db() -> PostgresClient:
     """Database dependency injection"""
     db = PostgresClient(os.getenv("POSTGRES_URL"))
-    await db.connect()
-    return db
