7c506e2246c416dcac93cf5bd50e0518475eefea
0xearlier <Gj2U9IhVwX0@jiangytiuo.com>
2025-01-14 18:11:18 +0300

    update logging to include request IDs

--- src/utils/logging/structured_logger.py
+++ src/utils/logging/structured_logger.py
@@ -1,11 +1,8 @@
-
-import logging
 from pythonjsonlogger import jsonlogger
+import logging
+
 class StructuredLogger(logging.Logger):
     
     """JSON-structured logging with context support"""
     def __init__(self, name: str):
         formatter = jsonlogger.JsonFormatter(
-        )
-        handler.setFormatter(formatter)
-            '%(asctime)s %(levelname)s %(name)s %(message)s'
