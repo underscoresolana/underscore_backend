85e9c7b4b911ab13a4925a4bad18781ad38f290d
0xearlier <Gj2U9IhVwX0@jiangytiuo.com>
2025-01-16 19:17:54 +0300

    [FIX] fix bug in feature extractor scaling

--- src/core/processing/feature_extractor.py
+++ src/core/processing/feature_extractor.py
@@ -1,11 +1,9 @@
 from src.connectors.solana.transaction_parser import SolanaTransactionParser
+
 from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
 import pandas as pd
-
 class AdvancedFeatureGenerator:
     """Generates complex features from raw transaction data"""
     
     def __init__(self, degree: int = 2):
-        poly_features = self.poly.fit_transform(base_features)
-        self.poly = PolynomialFeatures(degree, interaction_only=True)
